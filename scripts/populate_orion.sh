a3ea7cc0547e5cabf51d943fc3643f02a72eb986
brunorilla <brunorilla9@gmail.com>
2023-06-18 23:30:06 -0300

    feat: improving script for DATABASE SEED

--- scripts/populate_orion.sh
+++ scripts/populate_orion.sh
@@ -1,184 +1,100 @@
-## Populate Orion with stations
+#!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" -d '{
-    "description": {
-        "metadata": {
-            "name": {
-                "value": "Olavarría 1"
+# Array of location-related words for station names
+station_names=(
+    "Buenos Aires"
+    "Córdoba"
+    "Rosario"
+    "Mendoza"
+    "La Plata"
+    "San Miguel de Tucumán"
+    "Mar del Plata"
+    "Salta"
+    "Santa Fe"
+    "San Juan"
+    "Resistencia"
+    "Neuquén"
+    "Formosa"
+    "San Salvador de Jujuy"
+    "Paraná"
+    "Posadas"
+    "Santiago del Estero"
+    "Corrientes"
+    "San Fernando del Valle de Catamarca"
+    "Viedma"
+)
+
+# Array of words for sensor descriptions
+sensor_descriptions=(
+    "Clima"
+    "Particulas de Aire"
+    "Calidad del Agua"
+    "Ruido Ambiental"
+    "Contaminación Lumínica"
+    "Radiación Solar"
+)
+
+# Generate and populate stations
+for ((i = 1; i <= 20; i++)); do
+    rand_index=$(( RANDOM % ${#station_names[@]} ))
+    station_name="${station_names[$rand_index]} Station"
+
+    station_sponsor="Sponsor $i"
+
+    station_data='{
+        "description": {
+            "metadata": {
+                "name": {
+                    "value": "'"$station_name"'"
+                },
+                "sponsoredBy": {
+                    "value": "'"$station_sponsor"'"
+                }
             },
-            "sponsoredBy": {
-                "value": "TELECOM ARGENTINA"
-            }
+            "value": "This is a test station"
         },
-        "value": "This is a test station"
-    },
-    "location": {
-        "coordinates": [
-            -36.88154,
-            -60.37399
-        ]
-    },
-    "user": {
-        "value": "user_1"
-    }
-}' 'http://localhost:3000/stations'
-
-curl -X POST -H "Content-Type: application/json" -d '{
-    "description": {
-        "metadata": {
-            "name": {
-                "value": "San Isidro 1"
-            },
-            "sponsoredBy": {
-                "value": "Municipalidad de San Isidro"
-            }
+        "location": {
+            "coordinates": [
+                -36.88154,
+                -60.37399
+            ]
         },
-        "value": "This is a test station"
-    },
-    "location": {
-        "coordinates": [
-            -34.468732, -58.513389
-        ]
-    },
-    "user": {
-        "value": "user_1"
-    }
-}' 'http://localhost:3000/stations'
+        "user": {
+            "value": "user_1"
+        }
+    }'
 
-curl -X POST -H "Content-Type: application/json" -d '{
-    "description": {
-        "metadata": {
-            "name": {
-                "value": "San Vicente 1"
-            },
-            "sponsoredBy": {
-                "value": "Municipalidad de San Vicente"
+    curl -X POST -H "Content-Type: application/json" -d "$station_data" 'http://localhost:3000/stations'
+done
+
+# Generate and populate sensors
+for ((i = 1; i <= 50; i++)); do
+    rand_index=$(( RANDOM % ${#sensor_descriptions[@]} ))
+    sensor_description="Sensor ${sensor_descriptions[$rand_index]}"
+
+    sensor_station="station_$(( (i-1) / 5 + 1 ))"
+
+    sensor_data='{
+        "station_id": "'"$sensor_station"'",
+        "description": {
+            "value": "'"$sensor_description"'",
+            "metadata": {
+                "air_quality_index": "0",
+                "humidity": "0",
+                "pollutants": {
+                    "co2": "0",
+                    "co": "0",
+                    "no2": "0",
+                    "o3": "0",
+                    "so2": "0",
+                    "pm2.5": "0",
+                    "pm10": "0"
+                },
+                "pressure": "00",
+                "temperature": "0"
             }
-        },
-        "value": "This is a test station"
-    },
-    "location": {
-        "coordinates": [
-            -35.024603, -58.423253
-        ]
-    },
-    "user": {
-        "value": "user_2"
-    }
-}' 'http://localhost:3000/stations'
+        }
+    }'
 
-## Populate Orion with Sensors
-
-
-curl -X POST -H "Content-Type: application/json" -d '{
-  "station_id": "station_1",
-  "description": {
-    "value": "Sensor de Delmers",
-    "metadata": {
-      "air_quality_index": "0",
-      "humidity": "0",
-      "pollutants": {
-        "co2": "0",
-        "co": "0",
-        "no2": "0",
-        "o3": "0",
-        "so2": "0",
-        "pm2.5": "0",
-        "pm10": "0"
-      },
-      "pressure": "00",
-      "temperature": "0"
-    }
-  }
-}
-' 'http://localhost:3000/sensors'
-
-curl -X POST -H "Content-Type: application/json" -d '{
-"station_id": "station_1",
-"description": {
-  "value": "Sensor de Deboras",
-  "metadata": {
-    "air_quality_index": "0",
-    "humidity": "0",
-    "pollutants": {
-      "co2": "0",
-      "co": "0",
-      "no2": "0",
-      "o3": "0",
-      "so2": "0",
-      "pm2.5": "0",
-      "pm10": "0"
-    },
-    "pressure": "00",
-    "temperature": "0"
-  }
-}
-}
-' 'http://localhost:3000/sensors'
-
-curl -X POST -H "Content-Type: application/json" -d '{
- "station_id": "station_2",
- "description": {
-   "value": "Sensor de Eugenios",
-   "metadata": {
-     "air_quality_index": "20",
-     "humidity": "10",
-     "pollutants": {
-       "co2": "30",
-       "co": "40",
-       "no2": "50",
-       "o3": "90",
-       "so2": "10",
-       "pm2.5": "20",
-       "pm10": "30"
-     },
-     "pressure": "10",
-     "temperature": "30"
-   }
- }
-}' 'http://localhost:3000/sensors'
-
-curl -X POST -H "Content-Type: application/json" -d '{
-"station_id": "station_2",
-"description": {
-  "value": "Sensor de Jorges",
-  "metadata": {
-    "air_quality_index": "20",
-    "humidity": "10",
-    "pollutants": {
-      "co2": "30",
-      "co": "40",
-      "no2": "50",
-      "o3": "90",
-      "so2": "10",
-      "pm2.5": "20",
-      "pm10": "30"
-    },
-    "pressure": "10",
-    "temperature": "30"
-  }
-}
-}' 'http://localhost:3000/sensors'
-
-curl -X POST -H "Content-Type: application/json" -d '{
-"station_id": "station_2",
-"description": {
-  "value": "Sensor de Brenos",
-  "metadata": {
-    "prueba_random": "20",
-    "air_quality_index": "20",
-    "humidity": "10",
-    "pollutants": {
-      "co2": "30",
-      "co": "40",
-      "no2": "50",
-      "o3": "90",
-      "so2": "10",
-      "pm2.5": "20",
-      "pm10": "30"
-    },
-    "pressure": "10",
-    "temperature": "30"
-  }
-}
-}' 'http://localhost:3000/sensors'
+    curl -X POST -H "Content-Type: application/json" -d "$sensor_data" 'http://localhost:3000/sensors'
+done
